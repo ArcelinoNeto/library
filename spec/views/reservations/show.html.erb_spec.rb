fdf73c137eab0a49ccfe84e8becb1178c7f51105
ArcelinoNeto <arcelino.neto@gmail.com>
2022-09-26 08:24:32 -0300

    Correção no teste de views

--- spec/views/reservations/show.html.erb_spec.rb
+++ spec/views/reservations/show.html.erb_spec.rb
@@ -2,18 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "reservations/show", type: :view do
   before(:each) do
-    # @reservation = FactoryBot.create(:reservation)
+    @reservation = FactoryBot.create(:reservation)
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Booking Date/)
-    expect(rendered).to match(/Return Date/)
-    expect(rendered).to match(/Booking Status/)
-    expect(rendered).to match(/book/)
-    expect(rendered).to match(//)
-    
-    binding.pry
+    expect(rendered).to match(/Booking date/)
+    expect(rendered).to match(/Return date/)
+    expect(rendered).to match(/Booking status/)
+    expect(rendered).to match(/Book/)
+    expect(rendered).to match(/User/)
     
   end
 end
